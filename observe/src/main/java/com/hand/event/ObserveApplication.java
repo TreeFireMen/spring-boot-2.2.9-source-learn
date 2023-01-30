6a63bfb2dc97d7b057fbb9f74d0c4660a93716b3
曾梓健 <zijian.zeng@hand-china.com>
2023-01-30 16:03:44 +0800

    事件监听:初始化项目

---
+++ observe/src/main/java/com/hand/event/ObserveApplication.java
@@ -0,0 +1,13 @@
+package com.hand.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObserveApplication.class, args);
+	}
+
+}
---
+++ observe/src/test/java/com/hand/event/ObserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hand.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
